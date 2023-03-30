5a8edaf301aff6bcb088920095edd1aac4a6d4e9
mimovnik-manjaro <jkwidzinski@gmail.com>
2023-03-30 19:46:11 +0200

    Sketch the execution order

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,18 +6,18 @@
 
 void Parser::parse() {
     while (std::cin.good()) {
-        MyString input = MyString::readString(",{\n");
-
+        MyString input = MyString::readString(",\n");
         input.trim();
         if (input.equals("????")) {
-            std::cout << "Parsecommands" << std::endl;
             parseCommands();
-        } else if (input.equals("{")) {
+        } else {
+            // sections.add(parseSection(input));
         }
     }
 }
 
 void Parser::parseCommands() {
+    std::cout << "Parsing commands" << std::endl;
     while (std::cin.good()) {
         MyString input = MyString::readString("\n");
         input.trim();
@@ -27,4 +27,49 @@ void Parser::parseCommands() {
         std::cout << "Command" << std::endl;
     }
     return;
+}
+
+// Section* Parser::parseSection(MyString firstInput) {
+void Parser::parseSection(MyString firstInput) {
+    if (firstInput.equals("{")) {
+        // global section
+        // return new Section(nullptr, parseAttributes());
+    }
+
+    // return new Section(parseSelectors(firstInput), parseAttributes());
+    return;
+}
+
+// List<Selector>* Parser::parseSelectors() {
+void Parser::parseSelectors(MyString firstInput) {
+    // List<Selector>* selectors = new List<Selector>();
+    // selectors->add(new Selector(firstInput));
+    while (std::cin.good()) {
+        MyString name = MyString::readString(",\n");
+        name.trim();
+        if (name.equals("{")) {
+            break;
+        }
+
+        // selectors->add(new Selector(name));
+    }
+    // return selectors;
+}
+
+// List<Attribute>* Parser::parseAttributes() {
+void Parser::parseAttributes() {
+    // List<Attribute>* attributes = new List<Attributes>();
+    while (std::cin.good()) {
+        MyString property = MyString::readString(":");
+        property.trim();
+        if (property.equals("}")) {
+            break;
+        }
+
+        MyString value = MyString::readString(";\n");
+        value.trim();
+
+        // attributes->add(new Attribute(property, value));
+    }
+    // return attributes;
 }
--- src/Parser.hpp
+++ src/Parser.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "MyString.hpp"
+
 class Parser {
    private:
     void parseCommands();
+
+    void parseSection(MyString string);
+
+    void parseSelectors(MyString firstSelector);
+
+    void parseAttributes();
 
    public:
     void parse();
